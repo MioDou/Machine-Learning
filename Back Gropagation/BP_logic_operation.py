85014b10844169da3a79ad98072c754a4d6c2804
MioDou <49541696+MioDou@users.noreply.github.com>
2019-08-16 09:56:56 +0800

    Create BP_logic_operation.py

---
+++ Back Gropagation/BP_logic_operation.py
@@ -0,0 +1,67 @@
+# -*- coding:utf-8 -*-
+
+import numpy as np
+
+def TanH(input_sum):
+    return np.tanh(input_sum)
+def TanH_derivative(input_sum):
+    return 1.0 - np.tanh(input_sum) * np.tanh(input_sum)
+def sigmoid(input_sum):
+    return 1/(1+np.exp(-input_sum))
+def sigmoid_derivative(input_sum):
+    return input_sum*(1-input_sum)
+
+class NeuralNetwork:
+    def __init__(self, layers, activation='TanH'):
+
+        if activation == 'Sigmoid':
+            self.activation = sigmoid
+            self.activation_derivative = sigmoid_derivative
+        elif activation == 'TanH':
+            self.activation = TanH
+            self.activation_derivative = TanH_derivative
+
+        self.weights = []
+        for i in range(1, len(layers)-1):
+            self.weights.append( (2*np.random.random((layers[i-1] + 1, layers[i] + 1 ))-1 ) * 0.25 )
+            self.weights.append( (2*np.random.random((layers[i] + 1, layers[i+1] ))-1 ) * 0.25 )
+
+    def fit(self, X, y, learning_rate=0.2, epochs = 10000):
+        X = np.atleast_2d(X)
+        X = np.column_stack((X, np.ones(len(X))))
+        y = np.array(y)
+
+        for k in range(epochs):
+            i = np.random.randint(X.shape[0])
+            a = [X[i]]
+            for l in range(len(self.weights)):
+                a.append(self.activation( np.dot(a[l], self.weights[l])) )
+
+            error = y[i] - a[-1]
+            deltas = [error * self.activation_derivative(a[-1])]
+            layerNum = len(a) - 2
+
+            for j in range(layerNum, 0, -1):
+                deltas.append(deltas[-1].dot(self.weights[j].T) * self.activation_derivative(a[j]))
+            deltas.reverse()
+            for i in range(len(self.weights)):
+                layer = np.atleast_2d(a[i])
+                delta = np.atleast_2d(deltas[i])
+                self.weights[i] += learning_rate * layer.T.dot(delta)
+
+    def predict(self, input_sum):
+        input_sum = np.array(input_sum)
+        temp = np.ones(input_sum.shape[0] + 1)
+        temp[0:-1] = input_sum
+        a = temp
+        for l in range(0, len(self.weights)):
+            a = self.activation(np.dot(a, self.weights[l]))
+        return a
+
+if __name__ == "__main__":
+    nn = NeuralNetwork([2, 2, 1], 'TanH')
+    x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+    y = np.array([0, 1, 1, 0])
+    nn.fit(x, y)
+    for i in [[0, 0], [0, 1], [1, 0], [1, 1]]:
+        print(i, nn.predict(i))
